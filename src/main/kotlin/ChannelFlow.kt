ceb36d97e7350d1ad77f4d009211aea66edced0d
JinYan Liu <jane.jinyan.liu@gmail.com>
2024-07-19 23:52:32 +0200

    20240719

---
+++ src/main/kotlin/ChannelFlow.kt
@@ -0,0 +1,45 @@
+package org.example
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.first
+
+data class User(val name: String)
+
+interface UserApi {
+    suspend fun takePage(pageNumber: Int): List<User>
+}
+
+class FakeUserApi : UserApi {
+    private val users = List(20) { User("User$it") }
+    private val pageSize: Int = 3
+
+    override suspend fun takePage(pageNumber: Int): List<User> {
+        delay(1000)
+        return users
+            .drop(pageSize * pageNumber)
+            .take(pageSize)
+    }
+}
+
+fun allUsersFlow(api: UserApi): Flow<User> = channelFlow {
+    var page = 0
+    do {
+        println("Fetching page $page")
+        val users = api.takePage(page++)
+        users.forEach { send(it) }
+    } while (users.isNotEmpty())
+}
+
+suspend fun main() {
+    val api = FakeUserApi()
+    val users = allUsersFlow(api)
+    val user = users
+        .first {
+            println("Checking $it")
+            delay(1000)
+            it.name == "User3"
+        }
+    println(user)
+}
